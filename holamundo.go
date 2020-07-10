b1e4f4afaec56f45e32e62d7456d098022ecd9b7
josejulian.bayon.next <josejulian.bayon.next@bbva.com>
2020-07-10 10:14:26 +0200

    Jugando con Valores

--- holamundo.go
+++ holamundo.go
@@ -3,7 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	//Ejemplos tratamiento Valores
 
 	fmt.Println("Hola mundo")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	fmt.Println("go" + "lang")
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 
 }
